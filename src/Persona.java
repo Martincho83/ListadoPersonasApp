e15018d613b928d2818649f9f801522800fb3252
Martincho83 <martinlamas83@gmail.com>
2024-07-12 21:59:22 -0300

    first commit

---
+++ src/Persona.java
@@ -0,0 +1,66 @@
+public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private  static int numeroPersonas = 0;
+
+    // Constructor vacio
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+    //Sobrecarga Constructor
+    public Persona(String nnombre, String tel, String email){
+        this(); //Se llama al constructor vacio
+        this.nombre = nnombre;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}' + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Martin Lamas","344567459","mar@gmail.com");
+        System.out.println(persona1);
+    }
+}
